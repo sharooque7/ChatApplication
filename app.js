17fe6749631188730b3baa2147cd82db415460c5
Sharooque7 <sharooquethoufiq@gmail.com>
2022-04-07 22:17:27 +0530

    16euee new feature update

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/',(req,res)=>{
+    res.send("Hi Sharooque new features")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
